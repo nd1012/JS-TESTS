adf06e8d533be635a74cf167c71af92d4f4c446d
nd <nd1012@users.noreply.github.com>
2022-01-23 14:45:02 +0100

    Updates
    * Switch trunk to version #2
    * Some code refactoring
    + `Tests.VERSION` property
    + Strict type assertion

--- tests/tests.tests.js
+++ tests/tests.tests.js
@@ -1,11 +1,14 @@
 class Tests_Tests extends Tests{
 	async Tests_Test(){
+		// Version number
+		this.AssertEqual(Tests.VERSION,2,true);
+
 		// Success
 		let res=await this.AssertNoExceptionAsync(async ()=>await (new Tests_Tests_Ok()).RunTests());
 		this.AssertNotEqual(res,null)
 			.AssertEqual(res.LastError,null)
 			.AssertEqual(res.Tests,1,true)
-			.AssertEqual(res.Assertions,17,true)
+			.AssertEqual(res.Assertions,19,true)
 			.AssertEqual(res.Benchmarks.size,1,true)
 			.Assert(res.Benchmarks.has('Ok_Test'))
 			.AssertType(res.Benchmarks.get('Ok_Test'),Array)
@@ -44,8 +47,10 @@ class Tests_Tests_Ok extends Tests{
 			.AssertNotEqual(0,false,true)
 			.AssertType(this,Tests)
 			.AssertType(true,'boolean')
+			.AssertType(true,'boolean',true)
 			.AssertNotType(this,Number)
 			.AssertNotType(this,'string')
+			.AssertNotType(this,Tests,true)
 			.AssertException(()=>{throw new TypeError()})
 			.AssertException(()=>{throw new RangeError()},RangeError)
 			.Assert(await this.AssertNoExceptionAsync(async ()=>true))
